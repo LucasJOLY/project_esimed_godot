1122eaed5472b79eef8962253d312a1cf194ae47
Lucas JOLY <lucas.joly.0903@gmail.com>
2025-04-29 20:36:23 +0200

    advance

--- game/lib/level_1.gd
+++ game/lib/level_1.gd
@@ -1,14 +1,38 @@
-class_name Level_1 extends Node3D
+class_name Level_1
+extends Node3D
 
-@export var key:String;
+@export var key: String
+@onready var world_environment: WorldEnvironment = $WorldEnvironment
 
+var env: Environment
+var transition_speed := 0.75
 
-# Called when the node enters the scene tree for the first time.
+# Valeurs cibles
+var target_fog_density := 0.0005
+var target_fog_sky_affect := 0.2
+var target_fog_light_energy := 1.0
+
 func _ready() -> void:
-	SimpleGrass.set_interactive(true);
-	pass # Replace with function body.
+	SimpleGrass.set_interactive(true)
+	env = world_environment.environment
+	# Init aux valeurs claires
+	env.fog_density = 0.001
+	env.fog_sky_affect = 0.5
+	env.fog_light_energy = 1.0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	env.fog_density = lerp(env.fog_density, target_fog_density, delta * transition_speed)
+	env.fog_sky_affect = lerp(env.fog_sky_affect, target_fog_sky_affect, delta * transition_speed)
+	env.fog_light_energy = lerp(env.fog_light_energy, target_fog_light_energy, delta * transition_speed)
+
+func _on_dark_zone_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		target_fog_density = 0.2
+		target_fog_sky_affect = 1.0
+		target_fog_light_energy = 0.1
+
+func _on_dark_zone_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		target_fog_density = 0.0005
+		target_fog_sky_affect = 0.2
+		target_fog_light_energy = 1.0
